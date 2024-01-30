d210d140504ba811c38aeb9d82e78753bb15c5c8
nahuel-fiocco <nahuelfiocco@gmail.com>
2024-01-30 09:46:01 -0300

    forgotpassword improved

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../estilos/ForgotPassword.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faIdBadge } from '@fortawesome/free-solid-svg-icons';
 
 function ForgotPassword() {
   return (
     <div className='forgotPassword-container text-center bg-dark text-light'>
-      <h1>Olvidó su contraseña?</h1>
-      <h2>Póngase en contacto con el administrador del servicio</h2>
-      <Link className='backToHome' to="/">Ir a la página de inicio</Link>
+      <h1>¿Olvidaste su contraseña?</h1>
+      <h3>Por favor contactate con el administrador del servicio</h3>
+      <button>
+        <FontAwesomeIcon icon={faIdBadge} />
+        <Link className='link text-dark' to="/login">Volver al login</Link>
+      </button>
     </div>
   );
 }
