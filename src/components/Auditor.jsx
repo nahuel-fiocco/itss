4a780cb1d27998b0527b3e3cc30cd7ae994f1d7b
nahuel-fiocco <nahuelfiocco@gmail.com>
2024-02-12 21:39:21 -0300

    updated

--- src/components/Auditor.jsx
+++ src/components/Auditor.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getFirestore, collection, getDocs, onSnapshot, getDoc, doc, writeBatch } from 'firebase/firestore';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/AuthContext.jsx';
 import { css } from '@emotion/react';
 import { BarLoader } from 'react-spinners';
 import '../estilos/Auditor.css';
